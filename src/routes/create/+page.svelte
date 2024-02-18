a39ac14a06c34e41dae787e0b591a9b9d85f2ae4
eyronjuude <actanael@up.edu.ph>
2024-02-19 00:12:31 +0800

    feat: add layout for `create` route

---
+++ src/routes/create/+page.svelte
@@ -0,0 +1,8 @@
+<h2>Insert template</h2>
+<textarea id="event_template" rows="5" cols="50"/>
+
+<h2>DP Blast Time</h2>
+<input id="event_date" type="date"/>
+
+<h2>Confirm your event</h2>
+<input id="create_event" type="button" value="Submit"/>
