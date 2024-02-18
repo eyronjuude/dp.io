58c5cbef166460b592f4c3b2a0e0b0621efc2ad0
eyronjuude <actanael@up.edu.ph>
2024-02-18 13:33:53 +0800

    chore: rearrange `id` and `type` landing page button properties

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
 <h2>Welcome to dp.io</h2>
 <h3>Join Facebook DP blasts with ease!</h3>
 
-<input type="button" id="start" value="Let's get started!" />
+<input id="dp_start" type="button" value="Let's get started!" />
