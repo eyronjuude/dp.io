772851cd29cd53ecef33661044bdaea1c4e44fc3
eyronjuude <actanael@up.edu.ph>
2024-02-18 13:30:29 +0800

    style: enhance landing page headings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,4 @@
-Welcome to dp.io <br />
+<h2>Welcome to dp.io</h2>
+<h3>Join Facebook DP blasts with ease!</h3>
 
-<input type="button" id="start" value="Login to Facebook" />
+<input type="button" id="start" value="Let's get started!" />
