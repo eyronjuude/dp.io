e6689e5a7a596660dc7bd8312f100a8511bc9809
eyronjuude <actanael@up.edu.ph>
2024-05-12 11:27:13 +0800

    feat: create banner component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -20,6 +20,11 @@
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+	// Svelte Components
+	import Banner from './Banner.svelte'
 </script>
+
+<Banner />
 
 <slot />
---
+++ src/routes/Banner.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { AppBar } from '@skeletonlabs/skeleton';
+</script>
+
+<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+	<svelte:fragment slot="lead">Profile</svelte:fragment>
+	dp.io
+	<svelte:fragment slot="trail">About dp.io</svelte:fragment>
+</AppBar>
