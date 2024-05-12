faef80d03a2fad7ef8574a602e2458cf258b953c
eyronjuude <actanael@up.edu.ph>
2024-05-12 11:39:17 +0800

    feat: add avatar component in banner

--- src/routes/Banner.svelte
+++ src/routes/Banner.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
     import { AppBar } from '@skeletonlabs/skeleton';
+    import { Avatar } from '@skeletonlabs/skeleton';
 </script>
 
 <AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
-	<svelte:fragment slot="lead">Profile</svelte:fragment>
+	<svelte:fragment slot="lead">
+        <Avatar background="bg-secondary-500" border="border-4 border-surface-300-600-token hover:!border-primary-500" cursor="cursor-pointer">
+            :)
+        </Avatar>
+    </svelte:fragment>
 	dp.io
 	<svelte:fragment slot="trail">About dp.io</svelte:fragment>
 </AppBar>
