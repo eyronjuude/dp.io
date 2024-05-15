1a21518bfa836edb881129e51899cdf406c27385
eyronjuude <actanael@up.edu.ph>
2024-05-15 10:33:29 +0800

    feat: add initial dashboard layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,17 @@
 <script>
 	import FacebookLogin from "./FacebookLogin.svelte";
+	import Dashboard from "./Dashboard.svelte";
+	let login = false;
 </script>
 
+{#if login}
 <div class="flex justify-center items-center">
 	<FacebookLogin />
 </div>
+
+{:else}
+<div class="flex flex-col p-8 gap-4 justify-center">
+	<Dashboard />
+</div>
+
+{/if}
---
+++ src/routes/Dashboard.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { Avatar } from '@skeletonlabs/skeleton';
+</script>
+
+<Avatar
+    class="self-center"
+    width="w-32 md:w-60"
+	border="border-4 border-surface-300-600-token hover:!border-primary-500"
+	cursor="cursor-pointer"
+/>
+
+<h1 class="text-2xl md:text-4xl self-center">My Name Is Faker</h1>
+
+<hr class="h-0.5 border-t-0 bg-neutral-100 dark:bg-white/10" />
+
+<h1 class="text-2xl md:text-4xl">My Events</h1>
