c1d86b2a6dc65a03e4b006f2e63d03031a49e068
eyronjuude <actanael@up.edu.ph>
2024-05-15 11:49:22 +0800

    feat: add stylish events component

--- src/lib/components/dashboard/Events.svelte
+++ src/lib/components/dashboard/Events.svelte
@@ -1,3 +1,11 @@
+<script>
+	import Trash from "$lib/icons/Trash.svelte";
+</script>
+
 {#each [...Array(3).keys()].map(i => i+1) as num}
-    <div class="card p-4">Event {num}</div>
+    <div class="flex card">
+        <div class="grow-0 p-4">Event {num}</div>
+        <div class="grow"></div>
+        <Trash />
+    </div>
 {/each}
